7709ed5a052d10fb23e49e65c35522fb5a7a8d81
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-11-12 02:31:54 +0600

    added one line import in code example

---
+++ test.py
@@ -0,0 +1,9 @@
+
+#print(*[i for i in range(0, 501, 5)], sep="\n")
+
+#from random import choices
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# random_numbers = choices(numbers, k=4)
+
+print(tuple(__import__("random").choices(range(10),  k=4)))
