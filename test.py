e6b57abd9d0d547217e6640e0d71282d6167cfa1
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-11-13 03:10:05 +0600

    added dictionaies folder

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
+# print(*[i for i in range(0, 501, 5)], sep="\n")
 
-#print(*[i for i in range(0, 501, 5)], sep="\n")
-
-#from random import choices
+# from random import choices
 
 # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # random_numbers = choices(numbers, k=4)
